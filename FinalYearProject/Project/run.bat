0aba0836c6f056266b2740840b7faecb5e519e2c
joehan1303 <joehanm10@gmail.com>
2022-09-23 18:48:44 +0530

    Changed Directory

--- FinalYearProject/Project/run.bat
+++ FinalYearProject/Project/run.bat
@@ -1,2 +1,2 @@
-python -m streamlit run c:\Users\joeha\OneDrive\Desktop\FinalYearProject\Project\app.py
+python -m streamlit run C:\Users\joeha\OneDrive\Desktop\DocuScan\FinalYearProject\Project\app.py
 pause
